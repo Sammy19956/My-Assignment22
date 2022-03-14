eed40e57d211145c4247e10bb0042005ee50ee5c
BLESSING07 <eseomokhiboria@gmail.com>
2022-03-14 16:34:08 +0100

    A new branch

---
+++ index.js
@@ -0,0 +1,59 @@
+let sub=document.getElementById('decrement');
+let add=document.getElementById('increment');
+let result=document.getElementById('number');
+let person=document.getElementById('person');
+let integer=0;
+
+sub.addEventListener('click', function(a){
+    a.preventDefault();
+    if (integer>1){
+      integer-=1;  result.innerText=integer;
+        person.innerText=" Person";
+    return;
+    } else{
+    result.innerText=integer;
+       person.innerText=" Person";
+    }
+    
+});
+
+add.addEventListener('click', function(b){
+    b.preventDefault();
+    if(integer<1000){
+    integer +=1;
+    result.innerHTML=integer;
+    person.innerText=" People"
+    return;}
+});
+
+let MyName=document.getElementById('name');
+let MyEmail=document.getElementById('email');
+let date=document.getElementById('day');
+let time=document.getElementById('clock');
+let counter=document.getElementById('number');
+let output=document.getElementById('output');
+let submit=document.querySelector('#submit');
+
+console.log(output);
+submit.addEventListener('click', validate)
+
+function validate(e){
+    e.preventDefault();
+    if(MyName.value==""){
+        output.innerText="**Username must be filled out!!!";
+        return;
+    }
+    if(MyEmail.value==""){
+        output.innerText="**Email must be filled out!!!";
+        return;
+    }
+    if(date.value==""){
+        output.innerText="**Date must be filled out!!!";
+        return;
+    } 
+    if(time.value==""){
+        output.innerText="**Date must be filled out!!!";
+        return;
+    } 
+}
+
